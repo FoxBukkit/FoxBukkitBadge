2c7434bd1226765d21a10024ad73b7c82ea8f7f1
Mark Dietzer <mriq91@gmail.com>
2015-11-10 15:21:20 +0100

    Badges, not badge

--- src/main/java/com/foxelbox/foxbukkit/badge/FoxBukkitBadge.java
+++ src/main/java/com/foxelbox/foxbukkit/badge/FoxBukkitBadge.java
@@ -4,10 +4,9 @@ import com.foxelbox.dependencies.config.Configuration;
 import com.foxelbox.foxbukkit.badge.commands.BAddCommand;
 import com.foxelbox.foxbukkit.badge.commands.BListCommand;
 import com.foxelbox.foxbukkit.badge.commands.BManageCommand;
-import com.foxelbox.foxbukkit.badge.commands.BadgeCommand;
+import com.foxelbox.foxbukkit.badge.commands.BadgesCommand;
 import com.foxelbox.foxbukkit.badge.database.DatabaseBadgeManager;
 import com.foxelbox.foxbukkit.badge.database.DatabaseConnectionPool;
-import com.sun.xml.internal.bind.annotation.OverrideAnnotationOf;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class FoxBukkitBadge extends JavaPlugin {
@@ -40,7 +39,7 @@ public class FoxBukkitBadge extends JavaPlugin {
         getServer().getPluginCommand("badd").setExecutor(new BAddCommand(this));
         getServer().getPluginCommand("blist").setExecutor(new BListCommand(this));
         getServer().getPluginCommand("bmanage").setExecutor(new BManageCommand(this));
-        getServer().getPluginCommand("badge").setExecutor(new BadgeCommand(this));
+        getServer().getPluginCommand("badges").setExecutor(new BadgesCommand(this));
 
         globalBadgeManager = new MergingBadgeManager();
         addBadgeManager(databaseBadgeManager);
--- src/main/java/com/foxelbox/foxbukkit/badge/commands/BadgesCommand.java
+++ src/main/java/com/foxelbox/foxbukkit/badge/commands/BadgesCommand.java
@@ -5,10 +5,10 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 
-public class BadgeCommand implements CommandExecutor {
+public class BadgesCommand implements CommandExecutor {
     private final FoxBukkitBadge plugin;
 
-    public BadgeCommand(FoxBukkitBadge plugin) {
+    public BadgesCommand(FoxBukkitBadge plugin) {
         this.plugin = plugin;
     }
 
