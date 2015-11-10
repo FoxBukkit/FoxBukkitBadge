99d765774ae8674a67edcc6eaafa2c54c9364882
Mark Dietzer <mriq91@gmail.com>
2015-11-10 20:07:56 +0100

    Add Badges command

--- src/main/java/com/foxelbox/foxbukkit/badge/commands/BadgesCommand.java
+++ src/main/java/com/foxelbox/foxbukkit/badge/commands/BadgesCommand.java
@@ -1,9 +1,12 @@
 package com.foxelbox.foxbukkit.badge.commands;
 
+import com.foxelbox.foxbukkit.badge.Badge;
+import com.foxelbox.foxbukkit.badge.BadgeDescriptor;
 import com.foxelbox.foxbukkit.badge.FoxBukkitBadge;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 
 public class BadgesCommand implements CommandExecutor {
     private final FoxBukkitBadge plugin;
@@ -13,7 +16,32 @@ public class BadgesCommand implements CommandExecutor {
     }
 
     @Override
-    public boolean onCommand(CommandSender commandSender, Command command, String s, String[] strings) {
-        return false;
+    public boolean onCommand(CommandSender commandSender, Command command, String s, String[] args) {
+        final Player target;
+        if(args.length > 0) {
+            target = plugin.getServer().getPlayer(args[0]);
+        } else {
+            target = (Player)commandSender;
+        }
+
+        if(target == null) {
+            plugin.sendMessageTo(commandSender, "Sorry, could not find player.");
+            return true;
+        }
+
+        plugin.sendMessageTo(commandSender, "Badges for player " + target.getName());
+
+        for(Badge badge : plugin.getGlobalBadgeManager().getBadgesForPlayer(target)) {
+            BadgeDescriptor badgeDescriptor = badge.getDescriptor();
+            if(badgeDescriptor.getMaxLevel() > 1) {
+                plugin.sendMessageTo(commandSender, badgeDescriptor.getName() + ": " + badgeDescriptor.getLevelName(badge.getLevel()));
+            } else {
+                plugin.sendMessageTo(commandSender, badgeDescriptor.getName());
+            }
+        }
+
+        plugin.sendMessageTo(commandSender, "--- END ---");
+
+        return true;
     }
 }
